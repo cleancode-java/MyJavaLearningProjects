d880ce771450840a17cc7ebd4b0e43905548357c
cleancode-java <sciencework1@gmail.com>
2025-06-13 16:08:46 +0100

    Create Main.java
    This class serves as the entry point of the program.
    It defines the input coefficients of a quadratic (or linear) equation,
    invokes the solver class to compute the roots,
    and prints the results to the console.

---
+++ QuadraticEquationSolver/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double a = 7;
+        double b = -6;
+        double c = -1;
+        DiscriminantSolver.solve(a, b, c);
+        }
+    }
